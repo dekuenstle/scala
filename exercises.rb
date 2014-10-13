3252a51e5a7e0620ffd72e90745705cb54cfe51a
Cai Yufei <cai@mathematik.uni-marburg.de>
2014-10-13 18:56:40 +0200

    delete rocco docs
    Too much work to make them functional. We will use github
    to display source code for now.

--- exercises.rb
+++
@@ -1,9 +0,0 @@
-EXERCISES = [
-  ['1-hello', [
-    ['1.1' , '1-scalac/Hello.scala'],
-    ['1.2' , '2-sbt/src/main/scala/Hello.scala'],
-    ['1.3' , '2-sbt/src/test/scala/HelloSpec.scala'],
-    ['1.4' , '3-adt/src/main/scala/Shapes.scala'],
-    ['test', '3-adt/src/test/scala/ShapesSpec.scala'],
-  ]],
-]
