9f7d4a0b9e4647407ac6fbb7cc89b9cc9032be8a
lasergans <david.elias.k@gmail.com>
2014-10-25 13:14:42 +0200

    added 2.2 test impelementations

--- 2-descent/src/test/scala/CombinatorsSpec.scala
+++ 2-descent/src/test/scala/CombinatorsSpec.scala
@@ -8,8 +8,6 @@
 
 import org.scalatest._
 
-/* UNCOMMENT AFTER COMPLETION OF 2.1
-
 class CombinatorsSpec extends FlatSpec with Combinators {
 
   /* Recall that by writing combinators taking functions as
@@ -23,8 +21,6 @@ class CombinatorsSpec extends FlatSpec with Combinators {
     val e2code = "product of 6 and sum of 4 and 3"
     assert(parseExp3(e1code) == Some((e1, "")))
     assert(parseExp3(e2code) == Some((e2, "")))
-
-    // Your tests here
   }
 
   "The `choice` combinator" should "work for `parseExp3`" in testExp(parseExp3)
@@ -32,7 +28,10 @@ class CombinatorsSpec extends FlatSpec with Combinators {
   /* Check whether any given parser agrees with the first parser
    * of products in Exercise 2.1.
    */
-  def testMul(parseMul: Parser[Exp]): Unit = ???
+  def testMul(parseMul: Parser[Exp]): Unit = {
+    val e1code = "product of 6 and sum of 4 and 3"
+    assert( parseMul(e1code) == parseMul(e1code) )
+  }
 
   "The `sequence` combinator" should "work for `parseMul3`" in testMul(parseMul3)
 
@@ -60,10 +59,38 @@ class CombinatorsSpec extends FlatSpec with Combinators {
   }
 
   /* Task 2.2.4: Replace `pending` by a real test for `oneOrMore`. */
-  "The `oneOrMore` combinator" should "build parsers of nonempty lists" in pending
+  "The `oneOrMore` combinator" should "build parsers of nonempty lists" in {
+    val nums = oneOrMore(exp)
+
+    assert(nums("abcdefg") == None)
+
+    assert(nums("1234 abcd") == Some((List(Num(1234)), " abcd")))
+
+    assert(nums("sum of 1 and 1sum of 2 and 2sum of 3 and 3rd power of 2") ==
+      Some(
+        ( List(
+            Add(Num(1), Num(1)),
+            Add(Num(2), Num(2)),
+            Add(Num(3), Num(3))
+          ),
+          "rd power of 2" )
+      )
+    )
+  }
+
 
   /* Task 2.2.5: Replace `pending` by a real test for `parse3`. */
-  "`parse3`" should "parse expressions with spaces between words" in pending
+  "`parse3`" should "parse expressions with spaces between words" in {
+    assert( parse3(" sum    of     1   and 1 ") == Add(Num(1), Num(1)) )
+  }
 }
 
-UNCOMMENT AFTER COMPLETION OF 2.1 */
+
+
+
+
+
+
+
+
+
