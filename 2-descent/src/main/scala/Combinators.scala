de620ef7cb6fa432f8d78116b7c3d048dae875eb
Cai Yufei <yufei.cai@uni-tuebingen.de>
2014-10-28 15:26:29 +0100

    correct method names in optional exercise
    [Eye-blistering detail]
    
    The task 2.2.3 instructs students to rewrite parser combinators
    in monadic style. `map` is not the monadic `bind`; `flatMap` is.
    Thus the original instruction is not correct.

--- 2-descent/src/main/scala/Combinators.scala
+++ 2-descent/src/main/scala/Combinators.scala
@@ -30,9 +30,8 @@
   *    http://www.scala-lang.org/api/current/index.html#scala.Option
   *
   *    Rewrite the combinators `choice`, `sequence` and
-  *    `zeroOrMore` in terms of the methods `getOrElse` and `map`
-  *    of the class `Option`. Alternatively, you may use `orElse`
-  *    and `flatMap`.
+  *    `zeroOrMore` in terms of the methods `orElse` and `flatMap`
+  *    of the class `Option`.
   *
   * 4. Implement the combinator `oneOrMore` and write tests for
   *    it in CombinatorsSpec.scala.
