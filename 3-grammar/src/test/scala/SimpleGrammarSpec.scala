6d8ef22f8e9474da02ccd0f6e8f508a00f8f29aa
lasergans <david.elias.k@gmail.com>
2014-11-17 17:52:25 +0100

    implemented 3.3

--- 3-grammar/src/test/scala/SimpleGrammarSpec.scala
+++ 3-grammar/src/test/scala/SimpleGrammarSpec.scala
@@ -1,6 +1,7 @@
 import org.scalatest._
 
 import SimpleGrammar._
+//import NaiveGrammar
 
 class SimpleGrammarSpec extends FlatSpec {
   it should "parse and simplify arithmetic expressions" in {
@@ -19,6 +20,21 @@ class SimpleGrammarSpec extends FlatSpec {
         Leaf('num, "1234"))))
   }
 
+  val ae2: Grammar =
+    Grammar(
+      start = exp,
+      rules = Map(
+        exp -> (add | mul),
+        add -> (sumOf ~ num ~ and ~ mul),
+        mul -> (productOf ~ num ~ and ~ (add | num))
+      )
+    )
   // Your tests here
-  "You" should "write more tests" in fail("not finished yet")
+  "You" should "write more tests" in  {
+    assert( parseAE("product of 3 and sum of 1 and product of 2 and 5") ==
+      Branch('mul,List(Leaf('num,"3"), Branch('add,List(Leaf('num,"1"), Branch('mul,List(Leaf('num,"2"), Leaf('num,"5")))))))  )
+
+    assert( parseAE("product of 3 and sum of 1 and product of 2 and 5") ==
+      parseGrammar(ae2)("product of 3 and sum of 1 and product of 2 and 5"))
+  }
 }
